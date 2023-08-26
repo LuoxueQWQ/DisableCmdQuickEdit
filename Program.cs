464a376a0038c9daaa8b968f9a9c75e41c052415
-Luo- <82682041+LuoxueQWQ@users.noreply.github.com>
2023-08-26 22:51:00 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DisableCmdQuickEdit
+{
+    class Program
+    {
+        #region 关闭控制台 快速编辑模式、插入模式
+        const int STD_INPUT_HANDLE = -10;
+        const uint ENABLE_QUICK_EDIT_MODE = 0x0040;
+        const uint ENABLE_INSERT_MODE = 0x0020;
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern IntPtr GetStdHandle(int hConsoleHandle);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool GetConsoleMode(IntPtr hConsoleHandle, out uint mode);
+        [DllImport("kernel32.dll", SetLastError = true)]
+        internal static extern bool SetConsoleMode(IntPtr hConsoleHandle, uint mode);
+        public static void DisbleQuickEditMode()
+        {
+            IntPtr hStdin = GetStdHandle(STD_INPUT_HANDLE);
+            uint mode;
+            GetConsoleMode(hStdin, out mode);
+            mode &= ~ENABLE_QUICK_EDIT_MODE;//移除快速编辑模式
+            //mode &= ~ENABLE_INSERT_MODE;      //移除插入模式
+            SetConsoleMode(hStdin, mode);
+        }
+        #endregion
+
+        static void Main(string[] args)
+        {
+            DisbleQuickEditMode();
+        }
+    }
+}
---
+++ bin/Debug/1.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+echo.123
+pause
+
+call DisableCmdQuickEdit.exe
+
+set /p choice=ÀÊ“‚ ‰»Î: 
+
+echo.%choice%
+
+pause
